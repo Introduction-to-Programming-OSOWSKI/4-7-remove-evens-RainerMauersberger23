f89791d7772e4f9a83386f845090b45a846962d9
RainerMauersberger23 <829532@mystma.org>
2021-02-23 16:23:20 +0000

    Remove Evens Try 2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def removeEvens(k):
     newList = k
     numPopped = 0
     for i in range (0, len(k)):                                                                                   
-        if newList[i - numPopped] % 2 == 0:
+        if newList[i - numPopped] % 2 == 0 and newList[i - numPopped] != 0:
             newList.pop(i - numPopped)
             numPopped = numPopped + 1
     
